d7de3767bcc028e050223b189d59c98c94f1b8aa
JavierBernalGuerrero <jv_94@hotmail.com>
2016-09-22 09:11:25 +0200

    SaludoPersonalizado

--- holaMundo.py
+++ holaMundo.py
@@ -1,6 +1,6 @@
-# Saludo en Python
+# Hola mundo en Python
 
 print "Hola mundo!"
-print "Estoy aprendiendo Python"
-nombre = naw_input ("Dime como te llamas:")
-print "Encantado de saludarte", nombre
+print "Me esta gustando Python" 
+nombre = raw_input("Dime como te llamas: ")
+print "Encantado de saludarte ", nombre
